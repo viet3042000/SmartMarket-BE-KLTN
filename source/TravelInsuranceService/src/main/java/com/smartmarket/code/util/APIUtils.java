f876bfc73c8ca74def6584dfdb06d3ca2bcbed69
DESKTOP-SMPGCJP\QuốcViệt <viet3042000@gmail.com>
2021-06-11 17:46:02 +0700

    viet - commit

--- source/TravelInsuranceService/src/main/java/com/smartmarket/code/util/APIUtils.java
+++ source/TravelInsuranceService/src/main/java/com/smartmarket/code/util/APIUtils.java
@@ -12,7 +12,6 @@ import com.smartmarket.code.exception.APITimeOutRequestException;
 import com.smartmarket.code.exception.CustomException;
 import com.smartmarket.code.exception.HandleResponseException;
 import org.json.JSONObject;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
